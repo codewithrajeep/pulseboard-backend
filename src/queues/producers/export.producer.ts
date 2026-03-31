d3afa14b7c4100768cd87f5bf85889a9b1856dea
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-03-31 18:49:38 +0545

    feat(queues): add failed event handler to export worker

--- src/queues/producers/export.producer.ts
+++ src/queues/producers/export.producer.ts
@@ -1,9 +1,19 @@
 import { exportQueue } from "..";
 
 export const addExportJob = async (workspaceId: string, userId: string) => {
-  const job = await exportQueue.add("export-report", {
-    workspaceId,
-    userId,
-  });
+  const job = await exportQueue.add(
+    "export-report",
+    {
+      workspaceId,
+      userId,
+    },
+    {
+      attempts: 3,
+      backoff: {
+        type: "exponential",
+        delay: 1000,
+      },
+    },
+  );
   return job;
 };
