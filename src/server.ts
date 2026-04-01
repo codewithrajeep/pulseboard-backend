28d53955eb0d8e9f48513acf5032b7febd2783b9
Rajeep-Gadal <rajeepgadal@gmail.com>
2026-04-01 17:05:07 +0545

    refactor(server): import event listeners to initialize on startup

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import logger from "./utils/logger";
 import "./redis/index";
 import "./queues/workers/export.worker";
 import "./queues/workers/email.worker";
+import "./events/listeners/user.listener";
+import "./events/listeners/job.listener";
 
 const PORT = env.PORT;
 
